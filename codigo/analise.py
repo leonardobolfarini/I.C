91ea76aa5720bb3bf9cb5ffe09b2eb5cb37dc34a
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2024-11-10 02:54:45 -0300

    add: Add the new API function

--- codigo/analise.py
+++ codigo/analise.py
@@ -1,15 +1,16 @@
-import pandas as pd
 import csv
+import os
+import zipfile
+import pandas as pd
 from unidecode import unidecode
+from flask import Flask, request, send_file
 
-scopus_path_csv_original = 'arquivos/scopus-original.csv'
-scopus_path_csv = 'arquivos/scopus.csv'
-scopus_path_txt = 'arquivos/scopus.txt'
-wos_path_txt_original = 'arquivos/wos-original.txt'
-wos_path_txt = 'arquivos/wos.txt'
-wos_path_csv = 'arquivos/wos.csv'
-all_in_one_csv = 'all_in_one.csv'
-all_in_one_txt = 'all_in_one.txt'
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+OUTPUT_FOLDER = 'outputs'
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+os.makedirs(OUTPUT_FOLDER, exist_ok=True)
 
 header_csv = [
     "Authors", "Title", "Year", "Source title", "DOI", "Abstract", "Author Keywords"
@@ -41,16 +42,20 @@ columns_to_remove_csv = [
 def remove_columns(df, columns):
     return df.drop(columns=columns, errors='ignore').fillna("N/A")
 
-def process_wos_data(df):
+def process_wos_data(df, wos_path_txt, wos_path_csv):
     for column, new_index in columns_to_replace_txt:
         replaced_column_data = df[column].fillna("N/A")
         df.pop(column)
         df.insert(new_index, column, replaced_column_data)
         
+    import pandas as pd
+
     df["AU"] = df["AU"].apply(lambda x: '; '.join(
-        [' '.join(['.'.join([char for char in word]) + '.' if word.isupper() else word 
-                   for word in name.split()])
-         for name in x.split(';')]) if pd.notna(x) else x)
+        [' '.join(
+            [f'{word[0]}.{word[1:]}' if len(word) == 2 and word.isupper() else word
+            for word in name.split()]
+        ) + '.' if ',' in name else name for name in x.split(';')]) if pd.notna(x) else x)
+
     
     df["AU"] = df["AU"].apply(lambda x: x.replace(",", ""))
     
@@ -67,15 +72,15 @@ def process_wos_data(df):
 
     return df
 
-def process_scopus_data(df):
+def process_scopus_data(df, scopus_path, scopus_path_txt):
     data = []
-    with open(scopus_path_csv) as csv_scopus_file:
+    with open(scopus_path) as csv_scopus_file:
         for i, row in enumerate(csv.reader(csv_scopus_file, delimiter=',')):
             if i > 0:
                 data.append(row)
     
-    with open(scopus_path_txt, 'w', newline='') as csv_wos_file:
-        csv_writer = csv.writer(csv_wos_file, delimiter='\t')
+    with open(scopus_path_txt, 'w', newline='') as scopus_path_txt:
+        csv_writer = csv.writer(scopus_path_txt, delimiter='\t')
         csv_writer.writerow(header_txt)
         csv_writer.writerows(data)
 
@@ -91,7 +96,7 @@ def merge_and_process_files_in_csv(scopus_path, wos_path, output_path):
                 continue
             output_file.write(data)
     
-    df = pd.read_csv(output_path)
+    df = pd.read_csv(output_path, on_bad_lines='skip')
     
     for column in df.columns:
         if df[column].dtype == 'object':
@@ -105,7 +110,7 @@ def merge_and_process_files_in_csv(scopus_path, wos_path, output_path):
     df_without_duplicates = df[mask_to_keep]
     df_without_duplicates.to_csv(output_path, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
 
-    df = pd.read_csv(output_path)
+    df = pd.read_csv(output_path, on_bad_lines='skip')
     df_without_duplicates = df.drop_duplicates(subset=["Authors", "Title", "Source title"], keep='first')
     df_without_duplicates.to_csv(output_path, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
 
@@ -119,7 +124,7 @@ def merge_and_process_files_in_txt(scopus_path, wos_path, output_path):
                 continue
             output_file.write(data)
     
-    df = pd.read_csv(output_path, sep='\t')
+    df = pd.read_csv(output_path, sep='\t', on_bad_lines='skip')
     
     for column in df.columns:
         if df[column].dtype == 'object':
@@ -133,23 +138,50 @@ def merge_and_process_files_in_txt(scopus_path, wos_path, output_path):
     df_without_duplicates = df[mask_to_keep]
     df_without_duplicates.to_csv(output_path, sep='\t', index=False)
 
-    df = pd.read_csv(output_path, sep='\t')
+    df = pd.read_csv(output_path, sep='\t', on_bad_lines='skip')
     df_without_duplicates = df.drop_duplicates(subset=["AU", "TI", "SO"], keep='first')
     df_without_duplicates.to_csv(output_path, sep='\t', index=False)
 
-def main():
-    scopus_df = pd.read_csv(scopus_path_csv_original, sep=',')
-    scopus_df = process_scopus_data(scopus_df)
+@app.route('/process', methods=['Post'])
+def process_files():
+    if 'scopus_file' not in request.files or 'wos_file' not in request.files:
+        return "Arquivos de entrada necessários.", 400
+
+    scopus_file = request.files['scopus_file']
+    wos_file = request.files['wos_file']
+
+    scopus_path_csv = os.path.join(UPLOAD_FOLDER, 'scopus_file.csv')
+    scopus_path_txt = os.path.join(UPLOAD_FOLDER, 'scopus_file.txt')
+    wos_path_txt = os.path.join(UPLOAD_FOLDER, 'wos_file.txt')
+    wos_path_csv = os.path.join(UPLOAD_FOLDER, 'wos_file.csv')
+    output_csv_path = os.path.join(OUTPUT_FOLDER, 'all_in_one.csv')
+    output_txt_path = os.path.join(OUTPUT_FOLDER, 'all_in_one.txt')
+
+    scopus_file.save(scopus_path_csv)
+    wos_file.save(wos_path_txt)
+
+    scopus_df = pd.read_csv(scopus_path_csv, sep=',', on_bad_lines='skip')
+    scopus_df = process_scopus_data(scopus_df, scopus_path_csv, scopus_path_txt)
     scopus_df = remove_columns(scopus_df, columns_to_remove_csv)
     scopus_df.to_csv(scopus_path_csv, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
 
-    wos_df = pd.read_csv(wos_path_txt_original, sep='\t', on_bad_lines='skip')
-    wos_df = process_wos_data(wos_df)
+    wos_df = pd.read_csv(wos_path_txt, sep='\t', on_bad_lines='skip')
+    wos_df = process_wos_data(wos_df, wos_path_txt, wos_path_csv)
     wos_df = remove_columns(wos_df, columns_to_remove_txt)
     wos_df.to_csv(wos_path_txt, sep='\t', index=False)
     
-    merge_and_process_files_in_csv(scopus_path_csv, wos_path_csv, all_in_one_csv)
-    merge_and_process_files_in_txt(scopus_path_txt, wos_path_txt, all_in_one_txt)
+    merge_and_process_files_in_csv(scopus_path_csv, wos_path_csv, output_csv_path)
+    merge_and_process_files_in_txt(scopus_path_txt, wos_path_txt, output_txt_path)
+
+    return send_file(output_csv_path, as_attachment=True, download_name='all_in_one.csv')
+        # send_file(output_txt_path, as_attachment=True, download_name='all_in_one.txt')
+'''
+    zip_path = os.path.join(OUTPUT_FOLDER, 'resultados.zip')
+    with zipfile.ZipFile(zip_path, 'w') as zipf:
+        zipf.write(output_csv_path, arcname='all_in_one.csv')
+        zipf.write(output_txt_path, arcname='all_in_one.txt')
+
+    return send_file(zip_path, as_attachment=True)'''
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
