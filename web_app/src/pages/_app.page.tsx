900207d9ffdf56e699f14d232a3bdb7aa44770c9
leonardobolfarini <leonardobolfarini@gmail.com>
2024-11-16 19:59:05 -0300

    config: Configurate the react-query provider

--- web_app/src/pages/_app.page.tsx
+++ web_app/src/pages/_app.page.tsx
@@ -1,8 +1,14 @@
 import { globalStyles } from '@/src/styles/global'
 import type { AppProps } from 'next/app'
+import { QueryClientProvider } from 'react-query'
+import { queryClient } from '../lib/react-query'
 
 globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  )
 }
