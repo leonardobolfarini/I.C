2c380feacfe092c0b83da7852121dc3008aaa67a
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-02 17:43:32 -0300

    fix: Fixed the trouble from take the name as a component prop taking it from the event of select a file

--- web_app/src/components/FileInput/index.tsx
+++ web_app/src/components/FileInput/index.tsx
@@ -1,26 +1,40 @@
 import { MagnifyingGlass } from '@phosphor-icons/react/dist/ssr'
 import { FileInputLabel, FileInputStyle } from './styles'
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 
 interface FileInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   idhtml: string
-  filename: string | null
 }
 
-export const FileInput = forwardRef(function FileInput(
-  props: FileInputProps,
-  ref,
-) {
-  const { filename, idhtml } = props
-  return (
-    <div>
-      <FileInputLabel htmlFor={idhtml}>
-        <div>
-          <MagnifyingGlass width={20} height={20} weight="bold" />
-        </div>
-        {filename || 'Procurar arquivo'}
-      </FileInputLabel>
-      <FileInputStyle id={idhtml} type="file" {...props} ref={ref} />
-    </div>
-  )
-})
+export const FileInput = forwardRef<HTMLInputElement, FileInputProps>(
+  function FileInput(props: FileInputProps, ref) {
+    const { idhtml } = props
+
+    const [filename, setFilename] = useState<string | null>(null)
+
+    function handleFileChangeName(event: React.ChangeEvent<HTMLInputElement>) {
+      const file = event.target.files?.[0]
+      if (file) {
+        setFilename(file.name)
+      }
+    }
+
+    return (
+      <>
+        <FileInputLabel htmlFor={idhtml}>
+          <div>
+            <MagnifyingGlass width={20} height={20} weight="bold" />
+          </div>
+          {filename || 'Procurar arquivo'}
+        </FileInputLabel>
+        <FileInputStyle
+          id={idhtml}
+          type="file"
+          onChange={handleFileChangeName}
+          {...props}
+          ref={ref}
+        />
+      </>
+    )
+  },
+)
