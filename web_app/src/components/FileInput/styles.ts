558caf98267c31e6ee6869b6e823085ac0e26f19
leonardobolfarini <leonardobolfarini@gmail.com>
2025-01-02 17:41:21 -0300

    styles: Changed the none display that is causing trouble in input and add some other styles

--- web_app/src/components/FileInput/styles.ts
+++ web_app/src/components/FileInput/styles.ts
@@ -6,11 +6,15 @@ export const FileInputLabel = styled('label', {
   justifyContent: 'flex-start',
   padding: '6px 12px',
 
+  fontWeight: 'bold',
+  cursor: 'default',
+
   div: {
     cursor: 'pointer',
     border: '1px solid #ccc',
     borderRadius: 6,
-    padding: '2px',
+    padding: '4px',
+    lineHeight: 0,
 
     background: '#ccc',
     transition: 'background 0.2s',
@@ -22,5 +26,12 @@ export const FileInputLabel = styled('label', {
 })
 
 export const FileInputStyle = styled('input', {
-  display: 'none',
+  position: 'absolute',
+  width: '1px',
+  height: '1px',
+  padding: '0',
+  margin: '-1px',
+  overflow: 'hidden',
+  clip: 'rect(0,0,0,0)',
+  border: '0',
 })
