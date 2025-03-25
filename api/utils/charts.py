2b1be0dc68a533f2e82aacab391d4274759ca991
leonardobolfarini <leonardobolfarini@gmail.com>
2025-03-25 16:06:02 -0300

    feat: Created  the function that is used by the route to format the file into the chart format

---
+++ api/utils/charts.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import csv
+import utils.analysis_functions as analysis
+
+def chart_bar_formatter(file_path): 
+    file = pd.read_csv(file_path)
+    
+    tables_to_drop = ["Title","Year", "DOI", "Abstract"]
+    
+    file = analysis.remove_columns(file, tables_to_drop)
+    file.to_csv(file_path, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
+    
+    file_without_tables = pd.read_csv(file_path, sep=",")
+    authors_count = {}
+    keywords_count = {}
+    sources_count = {}
+    
+    for _, row in file_without_tables.iterrows():
+        author_list = str(row['Authors']).split(";")
+        for author in author_list:
+            author = author.strip()
+            if author not in authors_count:
+                authors_count[author] = 1
+            else:
+                authors_count[author] += 1
+        
+        keyword_list = str(row['Author Keywords']).split(";")
+        for keyword in keyword_list:
+            keyword = keyword.strip()
+            if keyword not in keywords_count:
+                keywords_count[keyword] = 1
+            else:
+                keywords_count[keyword] += 1
+                
+        source_list = str(row['Source title']).split(";")
+        for source in source_list:
+            source = source.strip()
+            if source not in sources_count:
+                sources_count[source] = 1
+            else:
+                sources_count[source] += 1
+        
+    chart_authors_data = [
+        {"author": author, "count": count}
+        for author, count in authors_count.items()
+        if author != "N/A"
+    ]
+    chart_keywords_data = [
+        {"keyword": keyword, "count": count}
+        for keyword, count, in keywords_count.items()
+        if keyword != "N/A"
+    ]
+    chart_source_data = [
+        {"source": source, "count": count}
+        for source, count, in sources_count.items()
+        if source != "N/A"
+    ]
+    
+    charts_data = [
+        { "authors": chart_authors_data },
+        { "keywords": chart_keywords_data },
+        { "sources": chart_source_data }
+    ]
+    
+    return charts_data
