6c1776a6ee1a5b3e8943d72ad8907f53fcc50d74
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-05-26 17:18:09 -0300

    refactor: Update data processing functions to improve column handling and add UTF-8 encoding for file operations

--- api/utils/analysis_functions.py
+++ api/utils/analysis_functions.py
@@ -5,30 +5,16 @@ from unidecode import unidecode
 header_csv = [
     "Authors", "Title", "Year", "Source title", "DOI", "Abstract", "Author Keywords"
 ]
+
 header_txt = [
     "AU", "TI", "PY", "SO", "DI", "AB", "DE"
 ]
-columns_to_replace_txt = [('AU', 0), ('TI', 1), ('PY', 2), ('SO', 3), ('DI', 4), ('AB', 5), ('DE', 6)]
-columns_to_remove_txt = [
-    'PT', 'BA', 'BE', 'GP', 'AF', 'BF', 'CA', 'SE', 'BS', 'LA', 'DT', 'CT', 'CY', 'CL',
-    'SP', 'HO', 'ID', 'C1', 'C3', 'RP', 'EM', 'RI', 'OI', 'FU', 'FP', 'FX', 'CR', 'NR', 
-    'TC', 'Z9', 'U1', 'U2', 'PU', 'PI', 'PA', 'SN', 'EI', 'BN', 'J9', 'JI', 'PD', 'VL', 
-    'IS', 'PN', 'SU', 'SI', 'MA', 'BP', 'EP', 'AR', 'EA', 'PG', 'WC', 'WE', 'SC', 'GA', 
-    'PM', 'OA', 'HC', 'HP', 'DA', 'UT', 'DL', 'D2'
-]
-columns_to_remove_csv = [
-    "Author full names", "Author(s) ID", "Link", "Authors with affiliations", 
-    "Molecular Sequence Numbers", "Volume", "Issue", "Art. No.", "Page start", 
-    "Page end", "Page count", "Cited by", "Affiliations", "Index Keywords", 
-    "References", "Chemicals/CAS", "Tradenames", "Correspondence Address", 
-    "Editors", "Publisher", "Sponsors", "Conference name", "Manufacturers", 
-    "CODEN", "Publication Stage", "Conference date", "Conference location", 
-    "Conference code", "ISSN", "ISBN", "PubMed ID", "Language of Original Document", 
-    "Abbreviated Source Title", "Document Type", "Open Access", "EID", "Source"
-]
 
-def remove_columns(df, columns):
-    return df.drop(columns=columns).fillna("N/A")
+columns_to_replace_txt = [('AU', 0), ('TI', 1), ('PY', 2), ('SO', 3), ('DI', 4), ('AB', 5), ('DE', 6)]
+
+def keep_columns(df, columns_to_keep):
+    existing_columns = [col for col in columns_to_keep if col in df.columns]
+    return df[existing_columns].fillna("N/A")
 
 def process_wos_data(df, wos_path_txt, wos_path_csv):
     for column, new_index in columns_to_replace_txt:
@@ -47,29 +33,29 @@ def process_wos_data(df, wos_path_txt, wos_path_csv):
     df.to_csv(wos_path_txt, sep='\t', index=False)
 
     data = []
-    with open(wos_path_txt) as txt_wosFile:
+    with open(wos_path_txt, encoding='utf-8-sig') as txt_wosFile:
         for i, row in enumerate(csv.reader(txt_wosFile, delimiter='\t', quotechar='"')):
             if i > 0:
                 data.append(row)
     
-    with open(wos_path_csv, 'w', newline='') as csv_wosFile:
+    with open(wos_path_csv, 'w', newline='', encoding='utf-8-sig') as csv_wosFile:
         csv_writer = csv.writer(csv_wosFile, delimiter=',', quoting=csv.QUOTE_ALL)
         csv_writer.writerow(header_csv)
         csv_writer.writerows(data)
 
 def process_scopus_data(scopus_path_csv, scopus_path_txt):
     data = []
-    with open(scopus_path_csv) as csv_scopusFile:
+    with open(scopus_path_csv, encoding='utf-8-sig') as csv_scopusFile:
         for i, row in enumerate(csv.reader(csv_scopusFile, delimiter=',')):
             if i > 0:
                 data.append(row)
-    with open(scopus_path_txt, 'w', newline='') as scopus_txt:
+    with open(scopus_path_txt, 'w', newline='', encoding='utf-8-sig') as scopus_txt:
         csv_writer = csv.writer(scopus_txt, delimiter='\t')
         csv_writer.writerow(header_txt)
         csv_writer.writerows(data)
 
 def merge_and_process_files_in_csv(scopus_path, wos_path, output_path):
-    with open(scopus_path, 'r') as scopusbibly, open(wos_path, 'r') as wos_data, open(output_path, 'a') as output_file:
+    with open(scopus_path, 'r', encoding='utf-8-sig') as scopusbibly, open(wos_path, 'r', encoding='utf-8-sig') as wos_data, open(output_path, 'a', encoding='utf-8-sig') as output_file:
         output_file.truncate(0)
         for i, data in enumerate(scopusbibly):
             output_file.write(data)
@@ -97,7 +83,7 @@ def merge_and_process_files_in_csv(scopus_path, wos_path, output_path):
     df_without_duplicates.to_csv(output_path, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
 
 def merge_and_process_files_in_txt(scopus_path, wos_path, output_path):
-    with open(scopus_path, 'r') as scopusbibly, open(wos_path, 'r') as wos_data, open(output_path, 'a') as output_file:
+    with open(scopus_path, 'r', encoding='utf-8-sig') as scopusbibly, open(wos_path, 'r', encoding='utf-8-sig') as wos_data, open(output_path, 'a', encoding='utf-8-sig') as output_file:
         output_file.truncate(0)
         for i, data in enumerate(wos_data):
             output_file.write(data)
