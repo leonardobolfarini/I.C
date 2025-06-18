4a7003e0bc6bb214d71bdfa95dd8ad570eb5d68d
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-06-18 15:33:18 -0300

    feat: Enhance chart data processing to include 'Year' for comprehensive analysis

--- api/utils/charts.py
+++ api/utils/charts.py
@@ -5,7 +5,7 @@ import utils.analysis_functions as analysis
 def chart_bar_formatter(file_path): 
     file = pd.read_csv(file_path)
     
-    tables_to_keep = ["Authors", "Author Keywords", "Source title"]
+    tables_to_keep = ["Authors", "Author Keywords", "Source title", "Year"]
     
     file = analysis.keep_columns(file, tables_to_keep)
     file.to_csv(file_path, sep=',', quotechar='"', quoting=csv.QUOTE_ALL, index=False)
@@ -14,6 +14,7 @@ def chart_bar_formatter(file_path):
     authors_count = {}
     keywords_count = {}
     sources_count = {}
+    years_count = {}
     
     for _, row in file_without_tables.iterrows():
         author_list = str(row['Authors']).split(";")
@@ -39,27 +40,41 @@ def chart_bar_formatter(file_path):
                 sources_count[source] = 1
             else:
                 sources_count[source] += 1
+
+        years_list = str(row['Year']).split(";")
+        for year in years_list:
+            year = year.strip()
+            if year not in years_count:
+                years_count[year] = 1
+            else:
+                years_count[year] += 1
         
     chart_authors_data = [
         {"author": author, "count": count}
         for author, count in authors_count.items()
-        if author != "N/A"
+        if author != "N/A" and author != "nan"
     ]
     chart_keywords_data = [
         {"keyword": keyword, "count": count}
         for keyword, count, in keywords_count.items()
-        if keyword != "N/A"
+        if keyword != "N/A" and keyword != "nan"
     ]
     chart_source_data = [
         {"source": source, "count": count}
         for source, count, in sources_count.items()
-        if source != "N/A"
+        if source != "N/A" and source != "nan"
+    ]
+    chart_years_data = [
+        {"year": year, "count": count}
+        for year, count, in years_count.items()
+        if year != "N/A" and year != "nan"
     ]
     
     charts_data = [
         { "authors": chart_authors_data },
         { "keywords": chart_keywords_data },
-        { "sources": chart_source_data }
+        { "sources": chart_source_data },
+        { "years": chart_years_data }
     ]
     
     return charts_data
