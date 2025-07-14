c2448c57793cc86a6bc456db60ae004371d8e2c5
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-07-14 17:17:33 -0300

    style: Update FileInputLabel and FileInputContent styles for improved layout and hover effects

--- web_app/src/components/FileInput/styles.ts
+++ web_app/src/components/FileInput/styles.ts
@@ -4,24 +4,30 @@ export const FileInputLabel = styled('label', {
   display: 'flex',
   alignItems: 'center',
   justifyContent: 'flex-start',
-  padding: '6px 12px',
 
-  gap: '0.25rem',
   fontWeight: 'bold',
   cursor: 'default',
 
+  width: '550px',
+  height: '300px',
+
   div: {
     cursor: 'pointer',
-    border: '1px solid #ccc',
-    borderRadius: 6,
-    padding: '4px',
-    lineHeight: 0,
+    padding: '20px',
+    background: '$white',
+  },
+})
 
-    background: '#ccc',
-    transition: 'background 0.2s',
+export const FileInputContent = styled('div', {
+  div: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: '1rem',
 
-    '&:hover': {
-      background: '#c4c4c4',
+    svg: {
+      backgroundColor: 'transparent',
     },
   },
 })
