41ac3ad956d6d2a2b09c70c54ef936888b975ac3
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2025-12-11 16:29:05 -0300

    fix: I add a uuid at the file name because if two people was using it maybe will had an error and all files are temporary to don't wast disk space

--- api/main.py
+++ api/main.py
@@ -1,5 +1,7 @@
 import csv
+import io
 import os
+import uuid
 import zipfile
 
 import pandas as pd
@@ -34,41 +36,72 @@ def process_files():
     scopusFile = request.files["scopusFile"]
     wosFile = request.files["wosFile"]
 
-    scopus_path_csv = os.path.join(UPLOAD_FOLDER, "scopusFile.csv")
-    scopus_path_txt = os.path.join(UPLOAD_FOLDER, "scopusFile.txt")
-    wos_path_txt = os.path.join(UPLOAD_FOLDER, "wosFile.txt")
-    wos_path_csv = os.path.join(UPLOAD_FOLDER, "wosFile.csv")
-    output_csv_path = os.path.join(OUTPUT_FOLDER, "all_in_one.csv")
-    output_txt_path = os.path.join(OUTPUT_FOLDER, "all_in_one.txt")
+    requisition_id = str(uuid.uuid4())
 
-    scopusFile.save(scopus_path_csv)
-    wosFile.save(wos_path_txt)
+    scopus_path_csv = os.path.join(UPLOAD_FOLDER, f"scopusFile_{requisition_id}.csv")
+    scopus_path_txt = os.path.join(UPLOAD_FOLDER, f"scopusFile_{requisition_id}.txt")
+    wos_path_txt = os.path.join(UPLOAD_FOLDER, f"wosFile_{requisition_id}.txt")
+    wos_path_csv = os.path.join(UPLOAD_FOLDER, f"wosFile_{requisition_id}.csv")
+    output_csv_path = os.path.join(OUTPUT_FOLDER, f"all_in_one_{requisition_id}.csv")
+    output_txt_path = os.path.join(OUTPUT_FOLDER, f"all_in_one_{requisition_id}.txt")
+    zip_path = os.path.join(OUTPUT_FOLDER, f"resultados_{requisition_id}.zip")
 
-    wos_df = pd.read_csv(wos_path_txt, sep="\t")
-    wos_df = analysis.keep_columns(wos_df, analysis.header_txt)
-    wos_df.to_csv(wos_path_txt, sep="\t", index=False)
-    wos_df = analysis.process_wos_data(wos_df, wos_path_txt, wos_path_csv)
+    try:
+        scopusFile.save(scopus_path_csv)
+        wosFile.save(wos_path_txt)
+        wos_df = pd.read_csv(wos_path_txt, sep="\t")
+        scopus_df = pd.read_csv(scopus_path_csv, sep=",")
 
-    scopus_df = pd.read_csv(scopus_path_csv, sep=",")
-    scopus_df = analysis.keep_columns(scopus_df, analysis.header_csv)
-    scopus_df.to_csv(
-        scopus_path_csv, sep=",", quotechar='"', quoting=csv.QUOTE_ALL, index=False
-    )
-    scopus_df = analysis.process_scopus_data(scopus_path_csv, scopus_path_txt)
+        wos_df = analysis.keep_columns(wos_df, analysis.header_txt)
+        scopus_df = analysis.keep_columns(scopus_df, analysis.header_csv)
 
-    analysis.merge_and_process_files_in_csv(
-        scopus_path_csv, wos_path_csv, output_csv_path
-    )
-    analysis.merge_and_process_files_in_txt(
-        scopus_path_txt, wos_path_txt, output_txt_path
-    )
+        wos_df.to_csv(wos_path_txt, sep="\t", index=False)
+        wos_df = analysis.process_wos_data(wos_df, wos_path_txt, wos_path_csv)
 
-    zip_path = os.path.join(OUTPUT_FOLDER, "resultados.zip")
-    with zipfile.ZipFile(zip_path, "w") as zipf:
-        zipf.write(output_csv_path, arcname="all_in_one.csv")
-        zipf.write(output_txt_path, arcname="all_in_one.txt")
+        scopus_df.to_csv(
+            scopus_path_csv, sep=",", quotechar='"', quoting=csv.QUOTE_ALL, index=False
+        )
+        scopus_df = analysis.process_scopus_data(scopus_path_csv, scopus_path_txt)
 
-    return send_file(zip_path, as_attachment=True)
+        analysis.merge_and_process_files_in_csv(
+            scopus_path_csv, wos_path_csv, output_csv_path
+        )
+        analysis.merge_and_process_files_in_txt(
+            scopus_path_txt, wos_path_txt, output_txt_path
+        )
+
+        with zipfile.ZipFile(zip_path, "w") as zipf:
+            zipf.write(output_csv_path, arcname="all_in_one.csv")
+            zipf.write(output_txt_path, arcname="all_in_one.txt")
+
+        with open(zip_path, "rb") as f:
+            data = io.BytesIO(f.read())
+
+        os.remove(zip_path)
+
+        return send_file(
+            data,
+            download_name=f"resultados_{uuid.uuid4()}.zip",
+            as_attachment=True,
+            mimetype="application/zip",
+        )
+
+    except Exception as e:
+        return f"Erro ao unir arquivos: {str(e)}", 500
+
+    finally:
+        files_to_remove = [
+            scopus_path_csv,
+            scopus_path_txt,
+            wos_path_txt,
+            wos_path_csv,
+            output_csv_path,
+            output_txt_path,
+        ]
+
+        for f in files_to_remove:
+            if os.path.exists(f):
+                os.remove(f)
 
 
 @app.route("/graph", methods=["Options", "Post"])
@@ -89,20 +122,18 @@ def get_graph_format():
     _, file_extension = os.path.splitext(filename)
     file_extension = file_extension.lower()
 
-    temp_filename = f"temp_graph_file{file_extension}"
-    graph_file_path = os.path.join(UPLOAD_FOLDER, temp_filename)
-
-    graphFile.save(graph_file_path)
+    unique_filename = f"{uuid.uuid4()}{file_extension}"
+    graph_file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
 
     try:
+        graphFile.save(graph_file_path)
         graph_data = graph.graph_formatter(graph_file_path, file_extension)
+        return graph_data
     except Exception as e:
         return f"Erro ao processar arquivo: {str(e)}", 500
     finally:
         if os.path.exists(graph_file_path):
             os.remove(graph_file_path)
-
-    return graph_data
 
 
 @app.route("/chart_bar", methods=["Options", "Post"])
@@ -123,20 +154,18 @@ def get_chart_format():
     _, file_extension = os.path.splitext(filename)
     file_extension = file_extension.lower()
 
-    temp_filename = f"temp_chart_bar{file_extension}"
-    chart_bar_file_path = os.path.join(UPLOAD_FOLDER, temp_filename)
-
-    chart_bar_file.save(chart_bar_file_path)
+    unique_filename = f"{uuid.uuid4()}{file_extension}"
+    chart_bar_file_path = os.path.join(UPLOAD_FOLDER, unique_filename)
 
     try:
+        chart_bar_file.save(chart_bar_file_path)
         chart_data = charts.chart_bar_formatter(chart_bar_file_path, file_extension)
+        return chart_data
     except Exception as e:
         return f"Erro ao processar arquivo: {str(e)}", 500
     finally:
         if os.path.exists(chart_bar_file_path):
             os.remove(chart_bar_file_path)
-
-    return chart_data
 
 
 if __name__ == "__main__":
