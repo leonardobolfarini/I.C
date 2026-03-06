2c5ea6d63ae0ffc0f430da9d2bd7c332415bef81
Leonardo Bolfarini <leonardobolfarini@gmail.com>
2026-03-06 13:47:47 -0300

    correção de linting e retornando algo quando não encontra alguma coluna

--- web_app/src/api/get-graph-format.ts
+++ web_app/src/api/get-graph-format.ts
@@ -1,19 +1,33 @@
-import { api } from '../lib/axios'
+import { api } from "../lib/axios";
 
 interface GetGraphFormatProps {
-  graphFile: File
+  graphFile: File;
+  graphType: "coauthorship" | "keywords";
 }
 
-export async function GetGraphFormat({ graphFile }: GetGraphFormatProps) {
-  const formData = new FormData()
-  formData.append('graphFile', graphFile)
+export async function GetGraphFormat({
+  graphFile,
+  graphType,
+}: GetGraphFormatProps) {
+  const formData = new FormData();
+  formData.append("graphFile", graphFile);
+  formData.append("graphType", graphType);
 
-  const response = await api.post('/graph', formData, {
+  const response = await api.post("/graph", formData, {
     headers: {
-      'Content-Type': 'multipart/form-data',
+      "Content-Type": "multipart/form-data",
     },
-    responseType: 'json',
-  })
+    responseType: "json",
+    validateStatus: (status) => {
+      return status < 500;
+    },
+  });
 
-  return response.data
+  if (response.status == 404) {
+    const graphTypeFormmated =
+      graphType == "coauthorship" ? "coautoria" : "palavras-chave";
+    alert(`Coluna de ${graphTypeFormmated} não encontrada no arquivo enviado.`);
+  }
+
+  return response.data;
 }
